c381a0be8eb4d91723ba365f34d0c3f8a2a4fa50
Laurent Tréguier <laurent@treguier.org>
2021-08-26 20:20:39 +0200

    Auto-logout only when authenticated

--- Fyreplace/Sources/ViewModels/ViewModel.swift
+++ Fyreplace/Sources/ViewModels/ViewModel.swift
@@ -12,7 +12,9 @@ protocol ViewModelDelegate: NSObjectProtocol {
 
 extension ViewModelDelegate {
     func onError(_ error: Error) {
-        guard let status = error as? GRPCStatus, status.code == .unauthenticated else {
+        guard let status = error as? GRPCStatus,
+              status.code == .unauthenticated,
+              Keychain.authToken.get() != nil else {
             return onFailure(error)
         }
 
