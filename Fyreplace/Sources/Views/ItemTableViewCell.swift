9a160868c58935560be013dfe52d413e9225d0d5
Laurent Tréguier <laurent@treguier.org>
2021-10-08 11:05:51 +0200

    Properly handle anonymous posts

--- Fyreplace/Sources/Views/ItemTableViewCell.swift
+++ Fyreplace/Sources/Views/ItemTableViewCell.swift
@@ -3,11 +3,11 @@ import SDWebImage
 
 class ItemTableViewCell: UITableViewCell {
     @IBOutlet
-    var date: UILabel!
+    var avatar: UIImageView!
     @IBOutlet
     var username: UILabel!
     @IBOutlet
-    var avatar: UIImageView!
+    var date: UILabel!
 
     private var textContent: UITextView!
     private var imageContent: UIImageView!
@@ -20,9 +20,18 @@ class ItemTableViewCell: UITableViewCell {
         dateFormatter.timeStyle = .medium
     }
 
-    func setup(at date: Date, from user: FPProfile) {
+    func setup(at date: Date, from profile: FPProfile) {
+        avatar.setAvatar(profile.avatar.url)
         self.date.text = dateFormatter.string(from: date)
-        username.text = user.username
-        avatar.setAvatar(user.avatar.url)
+
+        if profile.username.count == 0 {
+            let anonymous = NSAttributedString(
+                string: .tr("Anonymous"),
+                attributes: [.font: username.font.withTraits(.traitItalic)]
+            )
+            username.attributedText = anonymous
+        } else {
+            username.text = profile.username
+        }
     }
 }
