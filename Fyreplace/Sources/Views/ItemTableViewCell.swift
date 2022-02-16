4929fdd1ae866d56e032114b1ee4f25269534f49
Laurent Tréguier <laurent.treguier@fyreplace.app>
2022-02-16 21:46:13 +0100

    Ensure username is always set

--- Fyreplace/Sources/Views/ItemTableViewCell.swift
+++ Fyreplace/Sources/Views/ItemTableViewCell.swift
@@ -8,9 +8,13 @@ class ItemTableViewCell: AvatarTableViewCell {
     @IBOutlet
     var dateFormat: DateFormat!
 
+    override func setup(with profile: FPProfile) {
+        super.setup(with: profile)
+        username.setUsername(profile)
+    }
+
     func setup(at date: Date, with profile: FPProfile) {
         setup(with: profile)
         self.date.text = dateFormat.string(from: date)
-        username.setUsername(profile)
     }
 }
