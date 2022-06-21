8e37b64e19622aabaee2c1c24c72e47bdeccf311
Laurent Tréguier <laurent.treguier@fyreplace.app>
2022-06-21 21:18:29 +0200

    Use URL-safe characters for base64 encoding

--- Fyreplace/Sources/Extensions/Data.swift
+++ Fyreplace/Sources/Extensions/Data.swift
@@ -2,11 +2,17 @@ import Foundation
 
 extension Data {
     var base64ShortString: String {
-        return base64EncodedString().replacingOccurrences(of: "=", with: "")
+        return base64EncodedString()
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+            .replacingOccurrences(of: "=", with: "")
     }
 
     init?(base64ShortString: String) {
-        let paddedString = base64ShortString.padding(toLength: 24, withPad: "=", startingAt: 0)
+        let paddedString = base64ShortString
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+            .padding(toLength: 24, withPad: "=", startingAt: 0)
         self.init(base64Encoded: paddedString)
     }
 }
