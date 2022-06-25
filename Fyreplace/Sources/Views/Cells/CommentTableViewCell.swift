cf14cb40615bd7c96651c8eae8b4beae3f662674
Laurent Tréguier <laurent.treguier@fyreplace.app>
2022-06-25 20:07:06 +0200

    Show user profile on comment avatar click

--- Fyreplace/Sources/Views/Cells/CommentTableViewCell.swift
+++ Fyreplace/Sources/Views/Cells/CommentTableViewCell.swift
@@ -1,12 +1,23 @@
+import Kingfisher
 import UIKit
 
-class CommentTableViewCell: ItemTableViewCell {
+class CommentTableViewCell: UITableViewCell {
+    @IBOutlet
+    var avatar: UIButton!
+    @IBOutlet
+    var username: UIButton!
     @IBOutlet
     var content: UITextView!
+    @IBOutlet
+    var date: UILabel?
+    @IBOutlet
+    var dateFormat: DateFormat?
 
     func setup(with comment: FPComment, isPostAuthor: Bool) {
-        setup(at: comment.dateCreated.date, with: comment.author)
-        username?.textColor = isPostAuthor ? .init(named: "AccentColor") : .labelCompat
+        avatar?.setAvatar(comment.author.avatar.url)
+        username?.tintColor = isPostAuthor ? .init(named: "AccentColor") : .labelCompat
+        username?.setUsername(comment.author)
         content.text = comment.text
+        date?.text = dateFormat?.string(from: comment.dateCreated.date)
     }
 }
