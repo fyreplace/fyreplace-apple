4caad28b6f2e094c94fd09390407dccbda07d1c8
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-01-02 09:34:14 +0100

    Add vote and comment count to feed

--- Fyreplace/Sources/Views/Cells/FeedTableViewCell.swift
+++ Fyreplace/Sources/Views/Cells/FeedTableViewCell.swift
@@ -1,3 +1,4 @@
+import ReactiveSwift
 import SDWebImage
 import UIKit
 
@@ -8,9 +9,15 @@ class FeedTableViewCell: UITableViewCell {
     var down: UIButton!
     @IBOutlet
     var up: UIButton!
+    @IBOutlet
+    var votes: UILabel!
+    @IBOutlet
+    var comments: UILabel!
 
     private let feedbackGenerator = UISelectionFeedbackGenerator()
     private var isVoting = false
+    private var postId: Data?
+    private var trash: [Disposable] = []
 
     @IBAction
     func onDownPressed() {
@@ -26,6 +33,45 @@ class FeedTableViewCell: UITableViewCell {
         for button in [down, up] {
             button?.isEnabled = currentUser != nil
         }
+
+        votes.text = String(post.voteCount)
+        comments.text = String(post.commentCount)
+        postId = post.id
+
+        for disposable in trash {
+            disposable.dispose()
+        }
+
+        trash.append(NotificationCenter.default.reactive
+            .notifications(forName: AppDelegate.didReceiveRemoteNotificationNotification)
+            .take(during: reactive.lifetime)
+            .observe(on: UIScheduler())
+            .observeValues { [unowned self] in onAppDidReceiveRemoteNotification($0) }!)
+
+        trash.append(NotificationCenter.default.reactive
+            .notifications(forName: FPComment.wasCreatedNotification)
+            .take(during: reactive.lifetime)
+            .observe(on: UIScheduler())
+            .observeValues { [unowned self] in onCommentWasCreated($0) }!)
+    }
+
+    private func onAppDidReceiveRemoteNotification(_ notification: Notification) {
+        guard let info = notification.userInfo,
+              let command = info["_command"] as? String,
+              command == "comment:creation",
+              let postIdString = info["postId"] as? String,
+              let postId = Data(base64ShortString: postIdString),
+              postId == self.postId
+        else { return }
+        incrementCommentCount()
+    }
+
+    private func onCommentWasCreated(_ notification: Notification) {
+        guard let info = notification.userInfo,
+              let postId = info["postId"] as? Data,
+              postId == self.postId
+        else { return }
+        incrementCommentCount()
     }
 
     private func vote(with button: UIButton) {
@@ -39,6 +85,10 @@ class FeedTableViewCell: UITableViewCell {
             isVoting = false
             button.tintColor = .labelCompat
         }
+    }
+
+    private func incrementCommentCount() {
+        comments.text = String((Int(comments.text ?? "0") ?? 0) + 1)
     }
 }
 
