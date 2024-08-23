ef0efed8ecc0a3ab1a37c621f6124f3090ffba75
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-08-23 12:47:41 +0200

    Let iOS pick icon style

--- Fyreplace/Views/Navigation/Label+Destination.swift
+++ Fyreplace/Views/Navigation/Label+Destination.swift
@@ -2,12 +2,6 @@ import SwiftUI
 
 extension Label where Title == Text, Icon == Image {
     init(_ destination: Destination) {
-        #if os(macOS)
-            let iconSuffix = ""
-        #else
-            let iconSuffix = UIDevice.current.userInterfaceIdiom == .phone ? ".fill" : ""
-        #endif
-
-        self.init(destination.titleKey, systemImage: destination.icon + iconSuffix)
+        self.init(destination.titleKey, systemImage: destination.icon)
     }
 }
