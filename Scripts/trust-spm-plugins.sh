f67e294a7c184c8ce4637997f796cbb86a80b57d
Laurent Tréguier <laurent.treguier@fyreplace.app>
2025-08-17 16:01:07 +0200

    Fix uploading sources to Sentry

--- Scripts/trust-spm-plugins.sh
+++ Scripts/trust-spm-plugins.sh
@@ -3,7 +3,7 @@
 current_directory=$(dirname $0)
 security_directory=~/Library/org.swift.swiftpm/security
 
-cp $current_directory/../../Fyreplace.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved /tmp/packages.json
+cp $current_directory/../Fyreplace.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved /tmp/packages.json
 mkdir -p $security_directory
 
 node $current_directory/get-fingerprint.js \
---
+++ Scripts/upload-sentry-sources.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export PATH="$PATH:/opt/homebrew/bin"
+
+if [[ $CONFIGURATION -eq "Debug" ]]
+then
+    echo "Skipping Sentry sources upload"
+elif [ -n $SENTRY_AUTH_TOKEN ]
+then
+    ERROR=$(sentry-cli debug-files upload \
+        --org $SENTRY_ORG \
+        --project $SENTRY_PROJECT \
+        --auth-token $SENTRY_AUTH_TOKEN \
+        --include-sources "$DWARF_DSYM_FOLDER_PATH" \
+        --force-foreground 2>&1 >/dev/null)
+
+    if [[ $? -ne 0 ]]
+    then
+        echo "error: sentry-cli - $ERROR"
+        exit 1
+    fi
+fi
